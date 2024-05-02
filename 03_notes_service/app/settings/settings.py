8c7bc797bf110140d3676118c68c3a8ce3164b1b
iriskin77 <talcore@yandex.ru>
2024-05-02 13:33:52 +0500

    add: note service file structure

---
+++ 03_notes_service/app/settings/settings.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+
+load_dotenv()
+
+
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+
+
+DATABASE_URL_POSTGRES = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+print(DATABASE_URL_POSTGRES)
+
+engine = create_async_engine(DATABASE_URL_POSTGRES, echo=False, future=True)
+SessionLocal = sessionmaker(autoflush=False, bind=engine, class_=AsyncSession)
