9829547e4b375ec35b1dc3b3bc97127f4d1aa727
iriskin77 <talcore@yandex.ru>
2024-05-03 18:07:41 +0500

    add: new migrations and logger

---
+++ 03_notes_service/app/logger/logg.py
@@ -0,0 +1,21 @@
+import logging
+from typing import Literal
+
+
+def init_logger(name: str, level: int = logging.INFO):
+
+    if isinstance(level, str):
+        level = getattr(logging, level)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    console_log = logging.StreamHandler()
+    file_log = logging.FileHandler(filename='logs\\logs.log', mode='a')
+    console_log.setFormatter(logging.Formatter(
+        '%(asctime)s, — %(levelname)s — module: %(name)s — %(message)s'))
+    file_log.setFormatter(logging.Formatter(
+        '%(asctime)s, — %(levelname)s — module: %(name)s — %(message)s'))
+    logger.addHandler(console_log)
+    logger.addHandler(file_log)
+
+    return logger
