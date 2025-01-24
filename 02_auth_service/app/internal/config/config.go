81683cb27421ac580dcf7a83056757a511113aba
Vladislav <talcore@yandex.ru>
2025-01-24 10:26:46 +0500

    fix: config

--- 02_auth_service/app/internal/config/config.go
+++ 02_auth_service/app/internal/config/config.go
@@ -1,53 +1,94 @@
 package config
 
 import (
-	"flag"
+	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/spf13/viper"
 )
 
+type ConfigPostgres struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	NameDB   string
+	SSLMode  string
+}
+
+type ConfigRedis struct {
+	AddrRedis     string
+	PasswordRedis string
+	DBRedis       int
+}
+
+type ConfigServer struct {
+	Host     string
+	Port     string
+	LogLevel string
+}
+
+type ConfigToken struct {
+	JWTsecret       string
+	AccessTokenTTL  int
+	RefreshTokenTTL int
+}
+
+func fetchConfig() error {
+
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	return viper.ReadInConfig()
+
+}
+
 type Config struct {
-	BindAddr        string `yaml:"bind_ip"`
-	Port            string `yaml:"port"`
-	JWTSecret       string `yaml:"jwt_secret"`
-	Debug           bool   `yaml:"is_debug"`
-	Env             string `yaml:"env"`
-	AccessTokenTTL  int    `yaml:"access_tokenTTL"`
-	RefreshTokenTTL int    `yaml:"refresh_tokenTTL"`
+	Server   *ConfigServer
+	Postgres *ConfigPostgres
+	Redis    *ConfigRedis
+	Token    *ConfigToken
 }
 
-func LoadConfig(p string) *Config {
-	// path := fetchConfigPath()
-	// if path == "" {
-	// 	panic("config path is empty")
-	// }
+func NewConfig() (*Config, error) {
 
-	// if _, err := os.Stat(path); os.IsNotExist(err) {
-	// 	panic("config file does not exist " + path)
-	// }
-
-	var cfg Config
-
-	if err := cleanenv.ReadConfig(p, &cfg); err != nil {
-		panic("failed to read config: " + err.Error())
+	if err := fetchConfig(); err != nil {
+		fmt.Printf("error initialization config %s", err.Error())
+		return nil, err
 	}
 
-	return &cfg
-}
+	dbr, err := strconv.Atoi(os.Getenv("REDIS_DB"))
 
-// fetchConfigPath gets config path from command line flag or env variabl
-// Priority: flag > env > default
-// Default value is empty string
-func fetchConfigPath() string {
-	var res string
-
-	flag.StringVar(&res, "config", "", "path to config")
-	flag.Parse()
-
-	if res == "" {
-		res = os.Getenv("CONFIG_PATH")
+	if err != nil {
+		return nil, err
 	}
 
-	return res
+	return &Config{
+
+		Server: &ConfigServer{
+			Host:     viper.GetString("server.host"),
+			Port:     viper.GetString("server.port"),
+			LogLevel: viper.GetString("server.log_level"),
+		},
+		Token: &ConfigToken{
+			JWTsecret:       viper.GetString("token.jwt_secret"),
+			AccessTokenTTL:  viper.GetInt("token.access_token_TTL"),
+			RefreshTokenTTL: viper.GetInt("token.refresh_token_TTL"),
+		},
+
+		Postgres: &ConfigPostgres{
+			User:     os.Getenv("POSTGRES_USER"),
+			NameDB:   os.Getenv("POSTGRES_DB"),
+			Password: os.Getenv("POSTGRES_PASSWORD"),
+			Host:     os.Getenv("POSTGRES_HOST"),
+			Port:     os.Getenv("POSTGRES_PORT"),
+			SSLMode:  os.Getenv("SSLMODE"),
+		},
+		Redis: &ConfigRedis{
+			AddrRedis:     os.Getenv("REDIS_PORT"),
+			PasswordRedis: os.Getenv("REDIS_PASSWORD"),
+			DBRedis:       dbr,
+		},
+	}, nil
 }
