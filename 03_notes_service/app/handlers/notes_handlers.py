4fed92fe5bfe6f6de2838b3c3f563b0aa3d72be2
Vladislav <talcore@yandex.ru>
2025-02-08 21:22:29 +0500

    fix: note handlers

--- 03_notes_service/app/handlers/notes_handlers.py
+++ 03_notes_service/app/handlers/notes_handlers.py
@@ -2,18 +2,19 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from services.notes_services import INoteService
 from handlers.schema_request import NoteCreateRequest
-from container import container
+from container.container import container
 from sqlalchemy.ext.asyncio import AsyncSession
 from settings.async_session import get_async_session
 
 router_notes = APIRouter()
 
 
-@router_notes.get("/get_notes")
+@router_notes.get("/get_note")
 async def get_note_by_id(note_id: int,
                          async_session: AsyncSession = Depends(get_async_session),
                          notes_service: INoteService = Depends(container.get_notes_service)) -> JSONResponse:
-    note = notes_service.get_note_by_id(async_session=async_session, note_id=note_id)
+
+    note = await notes_service.get_note_by_id(async_session=async_session, note_id=note_id)
     if note is None:
         raise HTTPException(status_code=400, detail="Something wrong")
 
@@ -24,7 +25,8 @@ async def get_note_by_id(note_id: int,
 async def create_note(note: NoteCreateRequest,
                       async_session: AsyncSession = Depends(get_async_session),
                       notes_service: INoteService = Depends(container.get_notes_service)) -> JSONResponse:
-    note_id = notes_service.save_note(async_session=async_session, note=note.model_dump())
+
+    note_id = await notes_service.save_note(async_session=async_session, note=note.model_dump())
     if note_id is None:
         raise HTTPException(status_code=500, detail="Something wrong")
 
