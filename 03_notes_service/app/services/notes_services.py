fb64621e64fa07b6a692bd3ad4bae41a8e123bee
Vladislav <talcore@yandex.ru>
2025-02-08 21:22:05 +0500

    fix: save note service

--- 03_notes_service/app/services/notes_services.py
+++ 03_notes_service/app/services/notes_services.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 from repository.notes_repository import INoteRepository
+from repository.category_repository import ICategoryRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 from domain.domain import NoteEntity
 
@@ -7,25 +9,41 @@ from domain.domain import NoteEntity
 class INoteService(ABC):
 
     @abstractmethod
-    def get_note_by_id(self, async_session: AsyncSession, note_id: int) -> int | None:
+    async def get_note_by_id(self, async_session: AsyncSession, note_id: int) -> int | None:
         pass
 
     @abstractmethod
-    def save_note(self, async_session: AsyncSession, note: dict) -> int:
+    async def save_note(self, async_session: AsyncSession, note: dict) -> int:
         pass
 
 
 class NoteService(INoteService):
 
-    def __init__(self, notes_repo: INoteRepository):
+    def __init__(self, notes_repo: INoteRepository, categories_repo: ICategoryRepository) -> None:
         self.__notes_repo = notes_repo
+        self.__categories_repo = categories_repo
 
-    def get_note_by_id(self, async_session: AsyncSession, note_id: int) -> NoteEntity | None:
-        note = self.__notes_repo.get_note_by_id(async_session=async_session,
+    async def get_note_by_id(self, async_session: AsyncSession, note_id: int) -> NoteEntity | None:
+        note = await self.__notes_repo.get_note_by_id(async_session=async_session,
                                                 note_id=note_id)
         if note is not None:
-            return NoteEntity(note)
+            return NoteEntity(**note)
 
-    def save_note(self, async_session: AsyncSession, note: dict) -> int:
-        id_note = self.__notes_repo.save_note(async_session=async_session, note=note)
+    async def save_note(self, async_session: AsyncSession, note: dict) -> int:
+
+        category_id = await self.__categories_repo.get_category_id_by_name(async_session=async_session,
+                                                                           category_name=note["category_name"])
+        note.pop("category_name")
+        note["category_id"] = category_id
+
+        new_note = NoteEntity(
+            category_id=category_id,
+            user_id=note["user_id"],
+            title=note["title"],
+            body=note["body"],
+            update_at=int(datetime.now().timestamp()),
+            created_at=int(datetime.now().timestamp()),
+        )
+
+        id_note = await self.__notes_repo.save_note(async_session=async_session, note=new_note)
         return id_note
