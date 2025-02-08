248862284888d40a482d9495e1e1b154dbb29fee
Vladislav <talcore@yandex.ru>
2025-02-08 21:11:23 +0500

    add: methods to map domain entity

--- 03_notes_service/app/domain/domain.py
+++ 03_notes_service/app/domain/domain.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class NoteEntity:
-    note_id: int
+    id: Optional[int] = field(init=False, default=None)
     category_id: int
     user_id: int
     title: str
@@ -11,11 +12,25 @@ class NoteEntity:
     update_at: int
     created_at: int
 
+    def to_dict(self):
+        return self.__dict__
+
+    @classmethod
+    def to_model(cls, dict_obj):
+        return cls(**dict_obj)
+
 
 @dataclass
 class CategoryEntity:
-    category_id: int
+    id: Optional[int] = field(init=False, default=None)
     category_name: str
     user_id: str
     update_at: int
     created_at: int
+
+    def to_dict(self):
+        return self.__dict__
+
+    @classmethod
+    def to_model(cls, dict_obj):
+        return cls(**dict_obj)
