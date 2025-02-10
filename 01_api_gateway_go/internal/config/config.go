d09ee9ba61ca0a875601ded58438f1712fe6551c
Vladislav <talcore@yandex.ru>
2025-02-10 15:47:38 +0500

    add: notes server urls added in config

--- 01_api_gateway_go/internal/config/config.go
+++ 01_api_gateway_go/internal/config/config.go
@@ -12,6 +12,18 @@ type ConfigAuthServer struct {
 	UrlRefreshToken string
 }
 
+type ConfigNotesServer struct {
+	UrlCreateNote         string
+	UrlGetNoteByID        string
+	UrlGetNotes           string
+	UrlRemoveNoteByID     string
+	UrlRemoveNotes        string
+	UrlCreateCategory     string
+	UrlGetCategoryByID    string
+	UrlGetCategories      string
+	UrlRemoveCategoryByID string
+}
+
 type ConfigServer struct {
 	Host     string
 	Port     string
@@ -23,9 +35,10 @@ type ConfigToken struct {
 }
 
 type Config struct {
-	Server     *ConfigServer
-	AuthServer *ConfigAuthServer
-	Token      *ConfigToken
+	Server      *ConfigServer
+	AuthServer  *ConfigAuthServer
+	NotesServer *ConfigNotesServer
+	Token       *ConfigToken
 }
 
 func fetchConfig() error {
@@ -54,6 +67,17 @@ func NewConfig() (*Config, error) {
 			UrlCreateUser:   viper.GetString("auth_server.create_user_url"),
 			UrlLoginUser:    viper.GetString("auth_server.login_user_url"),
 			UrlRefreshToken: viper.GetString("auth_server.refresh_token_url"),
+		},
+		NotesServer: &ConfigNotesServer{
+			UrlCreateNote:         viper.GetString("notes_server.create_note_url"),
+			UrlGetNoteByID:        viper.GetString("notes_server.get_note_by_id"),
+			UrlGetNotes:           viper.GetString("notes_server.get_notes"),
+			UrlRemoveNoteByID:     viper.GetString("notes_server.remove_note_by_id"),
+			UrlRemoveNotes:        viper.GetString("notes_server.remove_notes"),
+			UrlCreateCategory:     viper.GetString("notes_server.create_category"),
+			UrlGetCategoryByID:    viper.GetString("notes_server.get_category_by_id"),
+			UrlGetCategories:      viper.GetString("notes_server.get_categories"),
+			UrlRemoveCategoryByID: viper.GetString("notes_server.remove_category_by_id"),
 		},
 		Token: &ConfigToken{
 			SigningKey: viper.GetString("token.jwt_secret"),
