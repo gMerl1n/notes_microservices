e166d00a267b86df5cdb96d608164a41ca762622
Vladislav <talcore@yandex.ru>
2025-02-11 14:51:15 +0500

    add: register notes get, create, remove handlers

--- 01_api_gateway_go/server/server.go
+++ 01_api_gateway_go/server/server.go
@@ -33,6 +33,10 @@ func NewHttpServer(ctx context.Context, log *logging.Logger, conf *config.Config
 
 	// notices handlers
 	router.HandleFunc("/api_gateway/v1/create_note", handlers.HandlersNotes.CreateNote).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/get_note_by_id", handlers.HandlersNotes.GetNoteByID).Methods("GET")
+	router.HandleFunc("/api_gateway/v1/get_notes", handlers.HandlersNotes.GetNotes).Methods("GET")
+	router.HandleFunc("/api_gateway/v1/delete_note_by_id", handlers.HandlersNotes.RemoveNoteByID).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/delete_notes", handlers.HandlersNotes.RemoveNotes).Methods("DELETE")
 
 	return &http.Server{
 		Addr:    conf.Server.Port,
