3ebfd216477954b4f0d4e3ac001741567dfa2f69
Vladislav <talcore@yandex.ru>
2025-02-12 23:46:55 +0500

    fix: init handler struct in server

--- 01_api_gateway_go/server/server.go
+++ 01_api_gateway_go/server/server.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func NewHttpServer(ctx context.Context, log *logging.Logger, conf *config.Config, validator *validator.Validate, jwtParser jwt.ITokenParser) (*http.Server, error) {
+func NewHttpServer(ctx context.Context, log *logging.Logger, conf *config.Config, validator *validator.Validate, jwtParser *jwt.TokenParser) (*http.Server, error) {
 
 	baseClient := client.NewBaseClient(log)
 
@@ -22,27 +22,27 @@ func NewHttpServer(ctx context.Context, log *logging.Logger, conf *config.Config
 	clients := clients.NewClient(baseClient, log, conf)
 
 	// Инициализация ручек
-	handlers := handlers.NewHandlers(clients.UserClient, clients.NotesClient, clients.CategoriesClient, jwtParser, validator, log)
+	handlers := handlers.NewHandler(clients.UserClient, clients.NotesClient, clients.CategoriesClient, jwtParser, validator, log)
 
 	router := mux.NewRouter()
 
 	// auth handlers
-	router.HandleFunc("/api_gateway/v1/create_user", handlers.HandlersUser.CreateUser).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/login_user", handlers.HandlersUser.LoginUser).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/refresh_token", handlers.HandlersUser.RefreshTokens).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/create_user", handlers.CreateUser).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/login_user", handlers.LoginUser).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/refresh_token", handlers.RefreshTokens).Methods("POST")
 
 	// notices handlers
-	router.HandleFunc("/api_gateway/v1/create_note", handlers.HandlersNotes.CreateNote).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/get_note_by_id", handlers.HandlersNotes.GetNoteByID).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/get_notes", handlers.HandlersNotes.GetNotes).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/delete_note_by_id", handlers.HandlersNotes.RemoveNoteByID).Methods("DELETE")
-	router.HandleFunc("/api_gateway/v1/delete_notes", handlers.HandlersNotes.RemoveNotes).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/create_note", handlers.CreateNote).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/get_note_by_id", handlers.AuthMiddleware(handlers.GetNoteByID)).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/get_notes", handlers.GetNotes).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/delete_note_by_id", handlers.RemoveNoteByID).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/delete_notes", handlers.RemoveNotes).Methods("DELETE")
 
 	// categories handlers
-	router.HandleFunc("/api_gateway/v1/create_category", handlers.HandlerCategories.CreateCategory).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/get_category_by_id", handlers.HandlerCategories.GetCategoryByID).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/get_categories", handlers.HandlerCategories.GetCategories).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/remove_category_by_id", handlers.HandlerCategories.RemoveCategoryByID).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/create_category", handlers.CreateCategory).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/get_category_by_id", handlers.GetCategoryByID).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/get_categories", handlers.GetCategories).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/remove_category_by_id", handlers.RemoveCategoryByID).Methods("DELETE")
 
 	return &http.Server{
 		Addr:    conf.Server.Port,
