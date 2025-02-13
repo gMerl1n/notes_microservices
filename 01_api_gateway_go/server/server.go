2d2543c50ac87cce5811f16a064707e2a464c982
Vladislav <talcore@yandex.ru>
2025-02-13 11:26:19 +0500

    add: middleware in notes handlers

--- 01_api_gateway_go/server/server.go
+++ 01_api_gateway_go/server/server.go
@@ -32,11 +32,11 @@ func NewHttpServer(ctx context.Context, log *logging.Logger, conf *config.Config
 	router.HandleFunc("/api_gateway/v1/refresh_token", handlers.RefreshTokens).Methods("POST")
 
 	// notices handlers
-	router.HandleFunc("/api_gateway/v1/create_note", handlers.CreateNote).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/create_note", handlers.AuthMiddleware(handlers.CreateNote)).Methods("POST")
 	router.HandleFunc("/api_gateway/v1/get_note_by_id", handlers.AuthMiddleware(handlers.GetNoteByID)).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/get_notes", handlers.GetNotes).Methods("POST")
-	router.HandleFunc("/api_gateway/v1/delete_note_by_id", handlers.RemoveNoteByID).Methods("DELETE")
-	router.HandleFunc("/api_gateway/v1/delete_notes", handlers.RemoveNotes).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/get_notes", handlers.AuthMiddleware(handlers.GetNotes)).Methods("POST")
+	router.HandleFunc("/api_gateway/v1/delete_note_by_id", handlers.AuthMiddleware(handlers.RemoveNoteByID)).Methods("DELETE")
+	router.HandleFunc("/api_gateway/v1/delete_notes", handlers.AuthMiddleware(handlers.RemoveNotes)).Methods("DELETE")
 
 	// categories handlers
 	router.HandleFunc("/api_gateway/v1/create_category", handlers.CreateCategory).Methods("POST")
